be1a87af7151605ee112f005989ba664f01624b0
harrydowning <harry.downing17@gmail.com>
2024-03-03 10:55:20 +0000

    feat: safe file-cache restore

--- src/file-cache.spec.ts
+++ src/file-cache.spec.ts
@@ -11,13 +11,14 @@ describe("file-cache", () => {
   const mockPath = "path";
   const mockData = Buffer.from("data");
 
-  it("correctly stores and restores a file that exists", () => {
+  it("correctly stores and restores a file that does exist", () => {
     existsSyncSpy.mockImplementationOnce(() => true);
     readFileSyncSpy.mockImplementationOnce(() => mockData);
 
     const fileCache = new FileCache();
     fileCache.store(mockPath);
 
+    expect(writeFileSyncSpy).toHaveBeenCalledTimes(0);
     expect(existsSyncSpy).toHaveBeenCalledTimes(1);
     expect(readFileSyncSpy).toHaveBeenCalledTimes(1);
     expect(readFileSyncSpy).toHaveBeenCalledWith(mockPath);
@@ -30,5 +31,19 @@ describe("file-cache", () => {
     fileCache.restore();
 
     expect(writeFileSyncSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("correctly stores and restores a file that doesn't exist", () => {
+    existsSyncSpy.mockImplementationOnce(() => false);
+
+    const fileCache = new FileCache();
+    fileCache.store(mockPath);
+
+    expect(existsSyncSpy).toHaveBeenCalledTimes(1);
+    expect(readFileSyncSpy).toHaveBeenCalledTimes(0);
+
+    fileCache.restore();
+
+    expect(writeFileSyncSpy).toHaveBeenCalledTimes(0);
   });
 });
